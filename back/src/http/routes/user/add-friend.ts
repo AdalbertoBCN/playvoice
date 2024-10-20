6dfe8e8bd10006dfea3a811aa06f70da1f9d769a
Iago <iagoclemilson@gmail.com>
2024-10-20 16:43:21 -0300

    PV-14,PV-12 ADICIONAR E BLOQUEAR USUÁRIOS

---
+++ back/src/http/routes/user/add-friend.ts
@@ -0,0 +1,37 @@
+import { z } from 'zod';
+import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import { prisma } from '../../../../prisma/prisma';
+
+export const addFriendRoutes: FastifyPluginAsyncZod = async function (app) {
+    app.put("/add-friend", {
+        schema: {
+            body: z.object({
+                id: z.string(),
+                friendId: z.string(),
+            }),
+        }
+    }, async (req) => {
+        const { id,friendId } = req.body
+
+        const user=await prisma.users.findUnique({
+            where:{
+                id
+            }
+        })
+        if (!user) throw new Error("usuario não encontrado")
+            
+        if (user.friends.includes(friendId)) throw new Error("você já é amigo deste usuário")
+            
+        if (user.blockedUsers.includes(friendId)) throw new Error("você bloqueou este usuário")
+    
+        await prisma.users.update({
+            where: {id},
+            data: {
+                friends:{
+                    push:friendId
+
+                }
+            }
+        });
+    })
+};
---
+++ back/src/http/routes/user/block-friend.ts
@@ -0,0 +1,35 @@
+import { z } from 'zod';
+import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import { prisma } from '../../../../prisma/prisma';
+
+export const blockFriendRoutes: FastifyPluginAsyncZod = async function (app) {
+    app.put("/block-friend", {
+        schema: {
+            body: z.object({
+                id: z.string(),
+                friendId: z.string(),
+            }),
+        }
+    }, async (req) => {
+        const { id,friendId } = req.body
+
+        const user=await prisma.users.findUnique({
+            where:{
+                id
+            }
+        })
+        if (!user) throw new Error("usuário não encontrado")
+            
+        if (user.blockedUsers.includes(friendId)) throw new Error("você já bloqueou este usuário")
+    
+        await prisma.users.update({
+            where: {id},
+            data: {
+                blockedUsers:{
+                    push:friendId
+
+                }
+            }
+        });
+    })
+};
